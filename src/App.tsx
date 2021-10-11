ce7f96011ace071076796a70d94b9dea7e62a0f5
Zhinar Masood Jihad <zeta404@Ohjelmointi-15-MacBook-Pro.local>
2021-10-11 13:35:58 +0300

    PortV1.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
 import React from 'react'
 import MyFirstComponent from './components/myFirstComponent';
 import SomeExampleComponent from './components/example/someExampleComponent';
-import ClickCount from './components/clickCount';
+import styles from './App.module.scss'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <MyFirstComponent />
-      <SomeExampleComponent />
-      <ClickCount/>
-    </div>
+
+   <Router basename='/portfolio'>
+      
+   {/* The navigation bar and other components you want to display on all pages come here */}
+   <div className={styles.navbar}>
+     <Link to='/'>Front page</Link>
+     <Link to='example'>Look my example component</Link>
+   </div>
+
+   <Switch>
+     {/* Changing content comes here */}
+     <Route exact path='/' component={MyFirstComponent} />
+     <Route exact path='/example' component={SomeExampleComponent}/>
+   </Switch>
+
+   {/* The footer and other components you want to display on all pages come here */}
+
+ </Router>
   )
 }
 
