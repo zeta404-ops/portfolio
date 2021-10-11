da20b12f33a985bb78dded0144ba15bbdaa4d91f
Zhinar Masood Jihad <zeta404@Ohjelmointi-15-MacBook-Pro.local>
2021-10-11 11:01:07 +0300

    PortV1.1

---
+++ src/components/jokunimen.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type JokunimenProps = {
+
+}
+
+const Jokunimen: React.FC<JokunimenProps> = () => {
+  return (
+    <div>
+      Jokunimen
+    </div>
+  )
+}
+
+export default Jokunimen
