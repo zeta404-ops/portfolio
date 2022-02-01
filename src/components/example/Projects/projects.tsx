1ba757c37327c35dc90261d2ed8bd6c8679d3a55
Zeta <83444918+zeta404@users.noreply.github.com>
2022-02-01 09:06:25 +0200

    PortV1.4

--- src/components/example/Projects/projects.tsx
+++ src/components/example/Projects/projects.tsx
@@ -18,32 +18,30 @@ const Project: React.FC<ProjectProps> = () => {
     <ReactBootStrap.Container>
   
     <ReactBootStrap.Nav className="me-auto">
-      <ReactBootStrap.Nav.Link href="#Home
-      ">Home</ReactBootStrap.Nav.Link>
+      <ReactBootStrap.Nav.Link href="#Home">Home</ReactBootStrap.Nav.Link>
       <ReactBootStrap.Nav.Link href="#CV">CV</ReactBootStrap.Nav.Link>
     </ReactBootStrap.Nav>
     </ReactBootStrap.Container>
-  </ReactBootStrap.Navbar>
+    </ReactBootStrap.Navbar>
   </div>
 </>
-<div className={styles.picBanner}>
-<img className="pic" src={projects} alt='projects banner'/>
-</div>
+  <div className={styles.picBanner}>
+  <img className="pic" src={projects} alt='projects banner'/>
+  </div>
 
 
-<div 
->
-<h3 className="zeta" >HERE YOU WILL SEE SOME OF MY PERSONAL PROJECTS</h3>
+  <div>
+    <h3 className={styles.zeta} >HERE YOU WILL SEE SOME OF MY PERSONAL PROJECTS</h3>
+    <div className='Gitrepos'>
+    <a className='ftw' href="https://github.com/zeta404-ops/FindTheUnknown"><img src="https://gh-card.dev/repos/zeta404-ops/FindTheUnknown.svg?fullname=" alt='ftw'/></a>
+    <br></br>
+    <a className='varia' href="https://github.com/varia-ict/varia-agile-20C-ryhma-3"><img src="https://gh-card.dev/repos/varia-ict/varia-agile-20C-ryhma-3.svg?fullname=" alt='ictvaria'/></a>
+    <br></br>
+    <a className='port' href="https://github.com/zeta404-ops/portfolio"><img src="https://gh-card.dev/repos/zeta404-ops/portfolio.svg?fullname=" alt='portfolio' /></a>
+    <br></br> 
+  </div>
 
-<a className='ftw' href="https://github.com/zeta404-ops/FindTheUnknown"><img src="https://gh-card.dev/repos/zeta404-ops/FindTheUnknown.svg?fullname=" alt='ftw'/></a>
-<br></br>
-<a className='varia' href="https://github.com/varia-ict/varia-agile-20C-ryhma-3"><img src="https://gh-card.dev/repos/varia-ict/varia-agile-20C-ryhma-3.svg?fullname=" alt='ictvaria'/></a>
-<br></br>
-<a className='port' href="https://github.com/zeta404-ops/portfolio"><img src="https://gh-card.dev/repos/zeta404-ops/portfolio.svg?fullname=" alt='portfolio' /></a>
-<br></br>
-
-
-</div>
+  </div>
 
 
   </div>
--- src/components/example/mainPage.tsx
+++ src/components/example/mainPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './mainpage.module.scss'
 import banner from '../video/bannerr.gif'
+import zz from '../video/ZZ.gif'
 import * as ReactBootStrap from "react-bootstrap"
 import {Link} from "react-router-dom";
 type MainPageProps = {
@@ -9,43 +10,43 @@ type MainPageProps = {
 
 const MainPage: React.FC<MainPageProps> = () => {
   return (
-    
     <div className={styles.container}>
-<>
-<div className={styles.nav}>
-  <ReactBootStrap.Navbar variant="light">
-    <ReactBootStrap.Container>
+
+      <div className={styles.nav}>
+        <ReactBootStrap.Navbar variant="light">
+          <ReactBootStrap.Container>
+            <Link to="/sd"> 
+            <div className={styles.logo} ><img src={zz} alt='logo'/> </div></Link>
+            <ReactBootStrap.Nav>
+            <ReactBootStrap.Nav.Link href="#Home">Home</ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href="#CV">CV</ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href="#Projects">Projects</ReactBootStrap.Nav.Link>
+
+          </ReactBootStrap.Nav>
+          </ReactBootStrap.Container>
+        </ReactBootStrap.Navbar>
+      </div>
   
-    <ReactBootStrap.Nav className="me-auto">
-      <ReactBootStrap.Nav.Link href="#Home
-      ">Home</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.Nav.Link href="#CV">CV</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.Nav.Link href="#Projects">Projects</ReactBootStrap.Nav.Link>
-    </ReactBootStrap.Nav>
-    </ReactBootStrap.Container>
-  </ReactBootStrap.Navbar>
+      {/* images for main page*/}
+
+      <div className={styles.pic}>
+      <img className={styles.pic} src={banner} alt='Banner for web dev'/>
+      
+      
+
+      {/* links for about me page*/}
+
+    </div>
+    <Link to="/aboutme"><p className={styles.explore} >Explore</p></Link>
+
+    <div>
+      <h3 className={styles.zeta} >Hello! Welcome to my portfolio profile.<br></br>
+        I am Zhinar Masood.<br></br>
+        I study information and communication technology and I'm excited about programming and cyber security.<br></br>
+        Here you will find my CV and some of my projects.
+      </h3>
+    </div>
   </div>
-  
-</>
-<div className={styles.picBanner}>
-<img className="pic" src={banner} alt='Banner for web dev'/>
-
-</div>
-<Link to="/aboutme"><p>Explore</p></Link>
-
-<div 
->
-<h3 className="zeta" >Hello! Welcome to my portfolio profile.<br></br>
-I am Zhinar Masood.<br></br>
-I study information and communication technology and I'm excited about programming and cyber security.<br></br>
-Here you will find my CV and some of my projects.</h3>
-
-</div>
-
-
-  </div>
-
-  )
-}
+)}
 
 export default MainPage
