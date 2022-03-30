7a595cfcd3d1b1bc3a5f5482d319ac96b2c558ce
Zeta <83444918+zeta404@users.noreply.github.com>
2022-03-30 20:56:11 +0300

    blank refresh fixed

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'reactjs-popup/dist/index.css';
-import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
