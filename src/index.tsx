7a21e99cc7c47f669beea8f230998fbc511c13b6
Zeta <83444918+zeta404@users.noreply.github.com>
2022-03-30 21:57:31 +0300

    fix blank refresh

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'reactjs-popup/dist/index.css';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Router basename='/portfolio'>
       <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
